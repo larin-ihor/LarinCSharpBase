6ca69d38d11bbaa78c1b1ff918fa24cd32d60541
larin-ihor <larin.ihor@gmail.com>
2020-04-05 22:34:57 +0300

    DZ6

--- Larin-CSharpCource/DZ6.cs
+++ Larin-CSharpCource/DZ6.cs
@@ -10,7 +10,88 @@ namespace HomeWork
     {
         public static void Start()
         {
+            int numberHomeWork;
+            do
+            {
+                Console.WriteLine("Введите номер задания:\n" +
+                    "1. Использование Стека\n" +
+                    "2. Использование очереди.\n" +
+                    "3. Дополнительно по Очереди.\n" +
+                    "");
+
+                int.TryParse(Console.ReadLine(), out numberHomeWork);
+
+                switch (numberHomeWork)
+                {
+                    case 1:
+                        UsingStack();
+                        continue;
+                    case 2:
+                        UsingQueue();
+                        continue;
+                    case 3:
+                        AdditionHomeWork();
+                        continue;
+                    default:
+                        break;
+                }
+
+            } while (numberHomeWork != 0);
+
+            UsingStack();
+
+            UsingQueue();
+
+        }
+
+        private static void UsingQueue()
+        {
+            //*****************************************************
+            //Queue
+            Console.WriteLine("\nQueue\n");
+
+            Queue<string> studentsCoffeQueue = new Queue<string>();
+
+            //наполнение стека
+            for (int i = 1; i <= 5; i++)
+            {
+                studentsCoffeQueue.Enqueue("Student" + i);
+                Console.WriteLine("Task from Student {0} > is received", i);
+            }
+
+            int studentsInStackQ = studentsCoffeQueue.Count;
+            for (int i = 1; i <= studentsInStackQ; i++)
+            {
+                Console.WriteLine("{0} got a cup of coffee", studentsCoffeQueue.Dequeue());
+            }
+            Console.WriteLine("\n");
+        }
+
+        private static void UsingStack()
+        {
+            //*******************************************************
+            //Stack
+            Console.WriteLine("\nStack\n");
+            Stack<string> studentsCoffeStack = new Stack<string>();
+            //наполнение стека
+            for (int i = 1; i <= 5; i++)
+            {
+                studentsCoffeStack.Push("Student" + i);
+                Console.WriteLine("Task from Student {0} > is received", i);
+            }
+
+            int studentsInStack = studentsCoffeStack.Count;
+            for (int i = 1; i <= studentsInStack; i++)
+            {
+                Console.WriteLine("{0} got a cup of coffee", studentsCoffeStack.Pop());
+            }
+            Console.WriteLine("\n");
+        }
+
+        private static void AdditionHomeWork()
+        {
 
         }
     }
+
 }
---
+++ Larin-CSharpCource/Work6.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    public static class Work6
+    {
+        public static void Start()
+        {
+            Stack<string> stack = new Stack<string>();
+
+            
+
+            int numb;
+
+            do
+            {
+                Console.WriteLine("Выберите метод работы со стеком\n" +
+                "0 - выход\n" +
+                "1 - добавление элемента\n" +
+                "2 - изъятие элемента");
+
+                int.TryParse(Console.ReadLine(), out numb);
+                switch (numb)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Введите что хотите внести в стек");
+                            string str = Console.ReadLine();
+                            if (!string.IsNullOrEmpty(str))
+                            {
+                                stack.Push(str);
+                            }    
+                        }
+                        continue;
+                    case 2:
+                        if (stack.Count > 0)
+                        {
+                            Console.WriteLine("Верхний элемент: " + stack.Pop());
+                        }
+                        else Console.WriteLine("Стек пустой");
+                        
+                        continue;
+                    default:
+                        break;
+                }
+            } while (numb != 0);
+
+            
+            //var nextInStack = stack.Pop();
+            //var peekResult = stack.Peek();
+
+            //Console.WriteLine(nextInStack);
+
+            //Queue<string> queue = new Queue<string>(new[] { "1", "2" });
+            //var nextInQueue = queue.Dequeue();
+
+
+
+            //Console.WriteLine(nextInQueue);
+        }
+    }
+}
