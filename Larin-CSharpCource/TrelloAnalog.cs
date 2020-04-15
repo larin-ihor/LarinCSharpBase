d1a53c3cc068a620301d93e90c73c12edd1f5156
larin-ihor <larin.ihor@gmail.com>
2020-04-15 10:29:15 +0300

    trello 1

---
+++ Larin-CSharpCource/TrelloAnalog.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class TrelloAnalog
+    {
+
+    }
+
+    public enum BoardStatus
+    {
+        ToDo,
+        OnTeacher,
+        OnStudent,
+        Done
+    }
+
+    class Person
+    {
+        public static int idCount;
+
+        public int id;
+        public string PersonName { get; set; }
+        
+        public Person(string personName)
+        {
+            id = ++idCount;    
+            PersonName = personName;
+        }
+        public Person(string personName, int idExist)
+        {
+            id = idExist;
+            PersonName = personName;
+        }
+    }
+
+    class Board
+    {
+        public static int idCount;
+
+        int id;
+
+        //public List<>
+    }
+
+    class HomeWork
+    {
+        public static int idCount;
+
+        int id;
+
+        Person Student;
+    }
+}
